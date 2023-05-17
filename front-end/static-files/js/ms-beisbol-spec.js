7262cca7886550678e6fbb913ceffe91f5419fb5
Miguel <mlb00033@red.ujaen.es>
2023-05-17 20:24:34 +0200

    Primer spec

--- front-end/static-files/js/ms-beisbol-spec.js
+++ front-end/static-files/js/ms-beisbol-spec.js
@@ -6,9 +6,52 @@
  */
 
 // SPECS para Jasmine
-describe("MAIN", function () {
-    it("main debería devolver true",
-        function () {
-            expect(true).toBeTrue();
-        });
+
+// Constantes para usar en las pruebas
+const elementoTituloPrueba = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_TITULO)
+const elementoContenidoPrueba = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_CONTENIDO)
+const TITULO_HOME = "Beisbol Home"
+const TITULO_ACERCA_DE = "Natacion Acerca de"
+
+const datosDescargadosPrueba = {
+    mensaje: "Mensaje de prueba descargado",
+    autor: "Prueba de autor",
+    email: "Prueba de email",
+    fecha: "00/00/0000"
+  }
+
+// Función para wait y dar tiempo a que responda el microservicio
+function wait(ms) {
+    var inicio = new Date().getTime();
+    var fin = 0;
+    while ((fin - inicio) < ms) {
+        fin = new Date().getTime();
+    }
+}
+
+
+
+// SPECS de prueba
+
+//Spec listarNombres
+//-----------------------------------------------------------------------------------------------------------
+//HU 02: Ver un listado solo con los nombres de todos los jugadores/equipos.---------------------------------
+describe("Prueba Jugadores.listadoNombres HU 02", function() {
+  beforeEach(function() {
+  // Le paso datos a  recupera() para que devuelva una lista de deportistas
+    spyOn(Jugadores, "recupera").and.callFake(function(callback) {
+      callback([
+        {nombre: "Miguel"},
+        {nombre: "Ilde"},
+        {nombre: "Alvaro"}
+      ]);
+    });
+  });
+  it("Se debería llamar a la función Jugadores.imprimeListadoNombres al ejecutar Jugadores.listadoNombres", function() {
+    spyOn(Jugadores, "imprimeListadoNombres");
+    
+    Jugadores.listadoNombres();
+    
+    expect(Jugadores.imprimeListadoNombres).toHaveBeenCalled();
+  });
 });
